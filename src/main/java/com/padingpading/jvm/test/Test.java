b9fd3d2bb22ffbbf1e8aaa6a37d8601c0ace73d3
libin <449196538@qq.com>
2021-07-04 09:56:53 +0800

    commit

---
+++ src/main/java/com/padingpading/jvm/test/Test.java
@@ -0,0 +1,12 @@
+package com.padingpading.jvm.test;
+
+/**
+ * @author libin
+ * @description
+ * @date 2021-07-04
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hahah");
+    }
+}
